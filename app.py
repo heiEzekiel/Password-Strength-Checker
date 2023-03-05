b976675025cdd41f9a09ff2f225ce78705d5789b
ndp1998 <vanguardlover537@gmail.com>
2023-03-06 02:51:00 +0800

    fixed CORS policy

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 from password_check import custom_password_req
+from flask_cors import CORS
+
 app = Flask(__name__)   
+CORS(app)
 
 @app.route("/")
 def home():
@@ -9,10 +12,7 @@ def home():
 @app.route("/custom_password", methods=["POST"])
 def custom_password():
     data = request.get_json(force=True)
-    print(data)
     password = data["id_password"]
-    print("hi")
-    print(password)
     result = str(custom_password_req(password)).lower()
     return jsonify(
             {
