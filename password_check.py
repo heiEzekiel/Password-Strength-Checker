8a362ee8daeca6f5a4cc155c9df76578dc79839a
kryeoh <kryeoh.2020@smu.edu.sg>
2023-03-09 17:27:12 +0800

    Squash commit
    fix: print output without having to click button
    
    fix: print entropy values on ui
    
    fix: added more pwd checks from zxcvbn
    
    fix: delete pycache
    
    fix: updated width of form
    
    fix: added static cross tick ui
    
    fix: made cross tick ui dynamic
    
    feat: created dynamic progress bar
    
    fix: update entropy, vulnerability+crack time css
    
    fix: tidied nits
    
    refactor code

--- password_check.py
+++ password_check.py
@@ -6,6 +6,7 @@ import string
 from collections import Counter
 from password_strength import PasswordPolicy
 from password_strength import PasswordStats
+from zxcvbn import zxcvbn
 
 def password_check(password):
     #Password Strength Lib
@@ -25,6 +26,17 @@ def password_check(password):
     pass
 
 def custom_password_req(password):
+    # create empty object to capture values
+    pwd_details = {
+        "hasLowerCase": False,
+        "hasUpperCase": False,
+        "hasSpChar": False,
+        "hasNumber": False,
+        "hasMinLength": False,
+        "hasNotBeenLeaked": False,
+        "hasNoSeqChar": False,
+    }
+
     # leaked password check
     sha1_password = hashlib.sha1(password.encode()).hexdigest().upper()
     sha1_prefix = sha1_password[:5]
@@ -33,56 +45,38 @@ def custom_password_req(password):
     response = requests.get(f"https://api.pwnedpasswords.com/range/{sha1_prefix}")
     if response.status_code != 200:
         return "Error: Could not check password against leaked passwords database."
-    
+
     leaked_passwords = response.text.split("\r\n")
     for temp_password in leaked_passwords:
-        if sha1_suffix in temp_password:
-            return "Weak password: Password has been leaked and should not be used."
+        if sha1_suffix not in temp_password:
+            pwd_details['hasNotBeenLeaked'] = True
         
     # length check
-    if len(password) < 8:
-        return "Weak password: Password must be at least 8 characters long."
+    if len(password) >= 8:
+        pwd_details['hasMinLength'] = True
 
-    # character types check
-    has_lowercase = False
-    has_uppercase = False
-    has_number = False
-    has_special = False
-    
-    # repeated characters check
-    # if len(set(password)) != len(password):
-    #     return "Weak password: Password should not contain repeated characters."
-    
     # sequential characters check
-    sequences = ["123", "234", "345", "456", "567", "678", "789", "890",
-                 "qwerty", "asdfgh", "zxcvbn", "qazwsx", "edcrfv", "tgbnhy"]
-    
-    for sequence in sequences:
-        if sequence in password.lower():
-            return "Weak password: Password should not contain sequential characters."
+    result = zxcvbn(password)
+
+    if 'sequence' in result:
+        sequences = result['sequence']
+        for seq in sequences:
+            if seq['pattern'] != 'sequence':
+                pwd_details['hasNoSeqChar'] = True
     
     # character types check (cont.)
 
     for char in password:
         if char.islower():
-            has_lowercase = True
+            pwd_details['hasLowerCase'] = True
         elif char.isupper():
-            has_uppercase = True
+            pwd_details['hasUpperCase'] = True
         elif char.isdigit():
-            has_number = True
+            pwd_details['hasNumber'] = True
         else:
-            has_special = True
+            pwd_details['hasSpChar'] = True
     
-    if not has_lowercase:
-        return "Weak password: Password must contain at least one lowercase letter."
-    elif not has_uppercase:
-        return "Weak password: Password must contain at least one uppercase letter."
-    elif not has_number:
-        return "Weak password: Password must contain at least one number."
-    elif not has_special:
-        return "Weak password: Password must contain at least one special character."
-    
-    return "Strong password"
+    return pwd_details
 
 def guessing_entropy(password):
     # Count the number of possible characters
@@ -111,10 +105,48 @@ def markov_model_entropy(password, order):
     entropy = -sum(p * math.log2(p) for p in probs)
     return entropy * len(password)
 
+def password_suggestion(password):
+    result = zxcvbn(password)
+    warning = result["feedback"]["suggestions"]
+    return warning
 
-# password = str(input("Password: "))
-# print("Guessing Entropy:", guessing_entropy(password))
-# print("Shannon Entropy:", shannon_entropy(password))
-# print("Markov Model Entropy:", markov_model_entropy(password, 2))
+def get_password_strength(password):
+    strength_checker = {
+        0: 'Very weak',
+        1: 'Weak',
+        2: 'Moderate',
+        3: 'Strong',
+        4: 'Very strong'
+    }
 
-# print(custom_password_req(password))
+    return strength_checker[zxcvbn(password)['score']]
+
+def get_crack_time(password):
+    
+    return zxcvbn(password)['crack_times_display']['offline_slow_hashing_1e4_per_second']
+
+def check_password_vulnerabilities(password):
+    # Use zxcvbn to check password vulnerabilities
+    result = zxcvbn(password)
+    isSusceptibleToAttacks = result['sequence'];
+    dictionaryWords = [];
+
+    if (len(isSusceptibleToAttacks) != 0):
+        for i in range(len(isSusceptibleToAttacks)):
+            if (isSusceptibleToAttacks[i]["pattern"]) == 'dictionary': 
+                dictionaryWords.append(isSusceptibleToAttacks[i]["token"]);
+            elif (isSusceptibleToAttacks[i]["pattern"]) == 'spatial':
+                return 'Your password is susceptible to spatial attacks.'
+            elif (isSusceptibleToAttacks[i]["pattern"]) == 'date':
+                return 'Your password is susceptible to date attacks.'
+            elif (isSusceptibleToAttacks[i]["pattern"]) == 'repeat':
+                return 'Your password is susceptible to repeat attacks.'
+            elif (isSusceptibleToAttacks[i]["pattern"]) == 'sequence':
+                return 'Your password is susceptible to sequence attacks.'
+            else:
+                return 'Your password is susceptible to brute force attacks.'
+        if (len(dictionaryWords)!= 0):
+            unpacked = ", ".join(dictionaryWords)
+            return unpacked + ' in password entered' + ' is susceptible to dictionary attacks.'
+    else: 
+        return 'Your password is not susceptible to attacks.'
