59d5a0e8a16f1c3823a729c2a59edad2d8eb23da
kryeoh <kryeoh.2020@smu.edu.sg>
2023-03-10 14:49:25 +0800

    fix: print entropy values on ui

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from password_check import custom_password_req
+from password_check import custom_password_req, guessing_entropy, shannon_entropy, markov_model_entropy
 from flask_cors import CORS
 
 app = Flask(__name__)   
@@ -13,11 +13,17 @@ def home():
 def custom_password():
     data = request.get_json(force=True)
     password = data["id_password"]
-    result = str(custom_password_req(password)).lower()
+    passwordStrength = custom_password_req(password)
+    guessingEntropy = guessing_entropy(password)
+    shannonEntropy = shannon_entropy(password)
+    markovEntropy = markov_model_entropy(password, 2)
     return jsonify(
             {
                 "code": 201,
-                "data": result,
+                "passwordStrength": passwordStrength,
+                "guessingEntropy": guessingEntropy,
+                "shannonEntropy": shannonEntropy,
+                "markovEntropy": markovEntropy,
                 "message": "Success"
             }
     ), 201
